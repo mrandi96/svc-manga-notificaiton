d4f3fd220720adcee816e3757bddc8fefee3f6e4
Muhammad Randi Pratama <mrandi96@gmail.com>
2021-09-13 09:05:18 +0700

    Update nodemailer.js

--- app/nodemailer.js
+++ app/nodemailer.js
@@ -1,16 +1,19 @@
 const nodemailer = require('nodemailer');
 
+const user = process.env.EMAIL;
+const pass = process.env.PASSWORD;
+
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'mrandi96@gmail.com',
-    pass: 'Gamelovers#01'
+    user,
+    pass
   }
 });
 
 const mainInformation = {
   name: 'Manga Notification',
-  address: 'mrandi96@gmail.com'
+  address: user
 };
 
 exports.sendMail = (to, subject, html) => {
@@ -20,10 +23,15 @@ exports.sendMail = (to, subject, html) => {
     subject,
     html
   };
+  
+  if (!user || !password) {
+    const error = new Error('please provide email and password for nodemailer');
+    throw error;
+  }
 
   transporter.sendMail(mailOptions, (e, info) => {
     if (e) {
-      console.log(e);
+      console.error(e);
       return e;
     }
     console.log(info);
